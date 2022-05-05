b712119bbb4ec6c389f6023ef91d2fe1c563f948
Luisa <luisa.h.morales@gmail.com>
2022-05-04 22:52:15 -0300

    descripciones mas largas añadidas

--- src/data/products.js
+++ src/data/products.js
@@ -3,13 +3,13 @@ module.exports=[
         id: 1,
         nombre: "Carpaccio fresco",
         precio: 65.50,
-        descripcion: "Entrada Carpaccio de salmón con cítricos"
+        descripcion: "El  carpaccio es un plato típico de la  cocina italiana. Nuestro plato esta elaborado a base de láminas muy finas de salmon , aderezadas con aceite de oliva, sal, cítricos y otros condimentos. Acompañado de lascas de queso parmesano. "
     },
     {
         id: 2,
         nombre: "Risoto de berenjena",
         precio: 47.00,
-        descripcion: "Risoto de berenjena y queso de cabra"
+        descripcion: "Otro típico plato italiano. Risoto de berenjena de coccion lenta con arroz de la variedad carnaroli y queso de cabra. Se sugiere acompañar este delicioso platillo con una copa de nuestro vino del mes, Rutini sauvignon blanc. "
     },
     {
         id: 3,
