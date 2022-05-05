d8538226026a91568c68ca427cfee178b4ba723b
Luisa <luisa.h.morales@gmail.com>
2022-05-04 21:41:30 -0300

    carpetas parciales implementadas en D.producto

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname,'..', 'public')));
 /* Telling the app to use the indexRouter when the user goes to the root of the website. */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
